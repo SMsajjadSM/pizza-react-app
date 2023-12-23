9fa74f97bdb0b038887f0a5c6e36313975e8eb08
sajjad <sajjadsiadati1390@gmail.com>
2023-12-23 13:05:52 +0330

    o some changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
+function App() {
+  return <h1>Hellow reactttttttttt</h1>;
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
